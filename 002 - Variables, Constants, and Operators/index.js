29213d4b9c815d6212184daaac87635a8cf64cda
Monis Amjad <monis.amjad@bcplatforms.com>
2025-02-07 19:49:53 +0200

    session 3

---
+++ 002 - Variables, Constants, and Operators/index.js
@@ -0,0 +1,143 @@
+// What is variable
+// What is constant
+// can start with |a to z, A to Z, underscore], $ then it could contain characters, number etc
+// DOM -> document object model
+
+// snake_case
+// camelCase
+// PASCALCASE
+
+// let, const, and var
+// Operators = *, /, -, +, +=, -=, *=, /=, ++, --
+/*
+
+let vegetables = 'onion' // declaration 
+
+vegetables = 'carrot' // assignment of the value 
+
+var BUCKET_OF_FRUITS = 123
+var fruits = 'Banana'
+
+fruits = 'apple'
+
+
+const PI = 3.142 // Fixed variable PI contain 3.
+
+var xyz = PI * 5 // error
+
+
+var stringType = 'a' // string
+var numberType = 1234 * 2 // number
+
+
+numberType = "Monis";
+
+
+var booleanType = true; // boolean true or false
+var b1234 = "Amjad";
+
+
+// Assignment Operator
+// Arthmetic operator
+
+var a = 5
+a = a + 7 // a = 6 , a += 7
+
+a = a + 1 
+//  is equal to 
+a += 1 
+// is equal to
+a++
+--a
+
+---
+
+a = a - 1
+a -= 1
+a--
+
+
+
+
+a += 
+
+a *= 5;
+a = a * 5;
+
+
+
+
+if, else, else if, switch
+
+
+var age = 1200
+var height = 170
+var weight = 70
+
+// Comparison operators
+> Greater than
+>= Greater than and equal to
+< Less than
+<= Less than and equal to
+== Equals to
+!= Not equal to
+! -> Not
+&& -> AND
+|| -> OR
+
+if (age == 20 ) {
+    if (height == 170 && weight === 70) {
+            console.log("condition is met")
+    }
+    if (height == 180 && weight == 80) {
+        console.log("condition is met")
+    }
+}
+
+
+if (!(age == 20 && height == 170 && weight === 70)) {
+    console.log("condition is met");
+    console.log("condition is met 2");
+} 
+
+
+let glassOfWaterToDrink = age > 60 ? 15 : 12; let age = 20; 
+
+if (age > 60) {
+    glassOfWaterToDrink = 15 
+    age = 20
+    
+} else {
+    glassOfWaterToDrink = 12;
+}
+
+CONDITION ? TRUE_PART : FALSE_PART -> ternary operator 
+
+; -> tell about ending of line
+
+if (0) {
+ //
+} else {
+
+}
+
+
+false, 0, "", -> false
+-1, 1, "asdad", true -> true
+
+var a = -1
+
+if (a) {
+    perfrom 
+}
+
+
+
+
+Scope of a variable 
+
+{
+ 
+
+}
+*/
